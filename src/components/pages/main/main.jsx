f43a00511db99446b74973f7440a189efc7684fa
Bob <noname.from.milkyway@gmail.com>
2024-09-26 11:52:25 +0300

    types block basically finished

--- src/components/pages/main/main.jsx
+++ src/components/pages/main/main.jsx
@@ -1,37 +1,11 @@
 import style from "./main.module.css";
-
+import Header from './header.jsx';
+import Types from "./sesTypes.jsx";
 export default function Main() {
     return (
         <>
-          <header className={style.header}>
-            <h1>ПРОЕКТИРОВАНИЕ, МОНТАЖ И НАЛАДКА<br /> СОЛНЕЧНЫХ ЭЛЕКТРОСТАНЦИЙ<br /> ЛЮБОЙ СЛОЖНОСТИ</h1>
-            <p style={{padding: '1em 0'}}>Стабильность, надежность и качество на каждом этапе.</p>
-            <div className={style.buttonContainer}>
-              <button className={style.buttonOrder}>ОПРОСНЫЙ ЛИСТ</button>
-              <button className={style.buttonContacts}>КОНТАКТЫ</button>
-            </div>
-            <p style={{padding: '1em 0 0'}}>Солнечные панели представляют собой эффективное и экологичное решение для снижения затрат на электроэнергию и снижения выбросов углекислого газа. Солнечная энергия является надежным источником возобновляемой энергии как для жилых, так и для промышленных целей. При минимальном техническом обслуживании и длительном сроке службы солнечные панели помогают вам добиться энергетической независимости, защитить от роста цен на электроэнергию и сделать планету более зеленой. Выбирайте солнечную энергию для получения более чистой, разумной и экономичной энергии сегодня и завтра.</p>
-          </header>
-          <div className={style.types}>
-            <div className={style.typesLeft}>
-              <div className={style.typesLeftL}>
-                <div style={{ display: 'flex'}}>
-                  <div className={style.orangeSquare}></div>
-                  <div className={style.blueTitle}><h6>Типы СЭС</h6></div>
-                </div>
-                <div className={style.typesLeftContent}>
-                  <h2>ОСНОВНЫЕ ТИПЫ <br/>ПРИМЕНЯЕМЫХ <br/>В НАСТОЯЩЕЕ ВРЕМЯ <span style={{color: '#ff8c00'}}>СЭС</span></h2>
-                  <p>Наши предложения <br/>по постройке солнечных <br/>электростанций</p>
-                </div>
-              </div>
-              <div className={style.typesLeftR}>
-                
-              </div>
-            </div>
-            <div className={style.typesRight}>
-
-            </div>
-          </div>
+          <Header />
+          <Types />
         </>
     )
 }
---
+++ src/components/pages/main/sesTypes.jsx
@@ -0,0 +1,109 @@
+import style from "./sesTypes.module.css";
+
+export default function Types() {
+    return (
+        <div className={style.types}>
+            <div className={style.typesLeft}>
+              <div className={style.typesLeftL}>
+                <div style={{ display: 'flex' }}>
+                  <div className={style.orangeSquare}></div>
+                  <div className={style.blueTitle}><h6>Типы СЭС</h6></div>
+                </div>
+                <div className={style.typesLeftContent}>
+                  <h2>ОСНОВНЫЕ ТИПЫ <br/>ПРИМЕНЯЕМЫХ <br/>В НАСТОЯЩЕЕ ВРЕМЯ <span style={{color: '#ff8c00'}}>СЭС</span></h2>
+                  <div style={{ backgroundColor: '#ff8c00', height: '4px', width: '25%', float: 'right', marginRight: '.5rem'}}></div>
+                  <p>Наши предложения <br/>по постройке солнечных <br/>электростанций</p>
+                </div>
+              </div>
+              <div className={style.typesLeftR}></div>
+            </div>
+            <div className={style.typesRight}>
+                <div className={style.typesRightT}>
+                    <div className={style.typresRightT1}>
+                    <h2>Сетевая СЭС</h2>
+                    <div style={{ backgroundColor: '#ff8c00', height: '4px', width: '50%'}}></div>
+                    </div>
+                    <div className={style.typresRightT2}>
+                    <h2>СЭС с резервным электропитанием</h2>
+                    <div style={{ backgroundColor: '#ff8c00', height: '4px', width: '50%'}}></div>
+                    </div>
+                    <div className={style.typresRightT3}>
+                    <h2>Автономная гибридная установка (АГЭУ)</h2>
+                    <div style={{ backgroundColor: '#ff8c00', height: '4px', width: '50%'}}></div>
+                    </div>
+                </div>
+                <div className={style.typesRightTn}>
+                    <div className={style.typresRightTn1}>
+                        <ul>
+                            <li>Фотоэлектрические модули (ФЭМ)</li>
+                            <li>Сетевой инвертор</li>
+                            <li>Устройство ограничения мощности (опция)</li>
+                            <li>Система мониторинга (опция)</li>
+                        </ul>
+                    </div>
+                    <div className={style.typresRightTn2}>
+                        <ul>
+                            <li>Фотоэлектрические модули (ФЭМ)</li>
+                            <li>Гибридный инвертор</li>
+                            <li>Система аккумуляторных батарей (АКБ)</li>
+                            <li>Устройство ограничения мощности (опция)</li>
+                            <li>Система мониторинга (опция)</li>
+                        </ul>
+                    </div>
+                    <div className={style.typresRightTn3}>
+                        <ul>
+                            <li>Фотоэлектрические модули (ФЭМ)</li>
+                            <li>Гибридный инвертор</li>
+                            <li>Система аккумуляторных батарей (АКБ)</li>
+                            <li>Дизельный или бензиновый электрогенератор</li>
+                            <li>Система мониторинга (опция)</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={style.typesRightB}>
+                    <h2>ПРЕИМУЩЕСТВА РАЗНЫХ ТИПОВ <span style={{color: '#ff8c00'}}>СЭС</span></h2>
+                </div>
+                <div className={style.typesRightTn}>
+                    <div className={style.typresRightTn1}>
+                        <ul>
+                            <li>Минимальные капитальные затраты</li>
+                            <li>Простота обслуживания</li>
+                            <li>Отсутствие аккумуляторных батарей</li>
+                            <li>Возможность подключения «зеленого тарифа» для продажи электроэнергии в сеть</li>
+                            <li>Низкая стоимость генерируемой СЭС электроэнергии</li>
+                        </ul>
+                    </div>
+                    <div className={style.typresRightTn2}>
+                        <ul>
+                            <li>Экономия потребляемой электроэнергии из сети</li>
+                            <li>Возможность подключения «зеленого тарифа» для продажи электроэнергии в сеть</li>
+                            <li>Возможность работы при пропадании электроснабжения</li>
+                        </ul>
+                    </div>
+                    <div className={style.typresRightTn3}>
+                        <ul>
+                            <li>Экономия денежных средств на техническом присоединении к электросетям</li>
+                            <li>Экономия потребления топлива дизельных электростанций на 50-80%</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={style.typesRightTn1}>
+                    <div className={style.typresRightTn1}>
+                        <button className={style.buttonTypesDesc}>ПОДРОБНЕЕ</button>
+                    </div>
+                    <div className={style.typresRightTn2}>
+                        <button className={style.buttonTypesDesc}>ПОДРОБНЕЕ</button>
+                    </div>
+                    <div className={style.typresRightTn3}>
+                        <button className={style.buttonTypesDesc}>ПОДРОБНЕЕ</button>
+                    </div>
+                </div>
+                <div className={style.typesRightB} style={{ display: 'flex', alignItems: 'flex-start' }}>
+                    <div style={{ backgroundColor: '#ff8c00', width: '8px', height: '100%', marginRight: '10px' }}></div>
+                    <p style={{textAlign: 'justify'}}>Товарищи! рамки и место обучения кадров представляет собой интересный эксперимент проверки новых предложений. Значимость этих проблем настолько очевидна, что новая модель организационной деятельности требуют определения и уточнения модели развития. Равным образом укрепление и развитие структуры играет важную роль в формировании системы обучения кадров, соответствует насущным потребностям.</p>
+                </div>
+            </div>
+
+        </div>
+    )
+} 
