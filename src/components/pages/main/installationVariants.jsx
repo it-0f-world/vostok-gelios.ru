3369561e1151338fb546b917dba82ce2dae24a5f
Bob Nem <bob.nem@ya.ru>
2024-09-26 17:31:12 +0300

    installation block basically finished

--- src/components/pages/main/installationVariants.jsx
+++ src/components/pages/main/installationVariants.jsx
@@ -13,16 +13,63 @@ export default function Types() {
             </div>
             <div className={style.bottom}>
                 <div className={style.b01}>
-                    <p>01</p>
+                    <div>
+                        <p>01</p>
+                        <h2>Крышная СЭС <br/>для плоской кровли</h2>
+                        <div style={{ backgroundColor: '#ff8c00', height: '4px', width: '20%'}}></div>
+                        <ul>
+                            <li>на 20% больше модулей на единицу площади</li>
+                            <li>минимизируется затенение </li>
+                            <li>увеличение генерации в утренние и вечерние часы</li>
+                            <li>удешевление опорной конструкции за счёт меньшей металлоёмкости</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <button className={style.buttonDesc}>УЗНАТЬ БОЛЬШЕ</button>
+                    </div>
                 </div>
                 <div className={style.b02}>
-                    <p>02</p>
+                    <div>
+                        <p>02</p>
+                        <h2>Крышная СЭС <br/>для парковок</h2>
+                        <div style={{ backgroundColor: '#ff8c00', height: '4px', width: '20%'}}></div>
+                        <ul>
+                            <li>современный и лаконичный дизайн</li>
+                            <li>короткое время сборки</li>
+                            <li>использование существующей инфраструктуры</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <button className={style.buttonDesc}>УЗНАТЬ БОЛЬШЕ</button>
+                    </div>
                 </div>
                 <div className={style.b03}>
-                    <p>03</p>
+                    <div>
+                        <p>03</p>
+                        <h2>Крышная СЭС <br/>для скатной кровли</h2>
+                        <div style={{ backgroundColor: '#ff8c00', height: '4px', width: '20%'}}></div>
+                        <ul>
+                            <li>возможна установка на все виды кровли</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <button className={style.buttonDesc}>УЗНАТЬ БОЛЬШЕ</button>
+                    </div>
                 </div>
                 <div className={style.b04}>
-                    <p>04</p>
+                    <div>
+                        <p>04</p>
+                        <h2><span style={{ color: '#ff8c00' }}>С</span>олнечная <span style={{ color: '#ff8c00' }}>Э</span>лектро <br/><span style={{ color: '#ff8c00' }}>С</span>танция на земле</h2>
+                        <div style={{ backgroundColor: '#ff8c00', height: '4px', width: '20%'}}></div>
+                        <ul>
+                            <li>простота конструкции</li>
+                            <li>для изготовления опорных конструкций используется горячеоцинкованная сталь с толщиной цинка 80-250 мкм</li>
+                            <li>высокая скорость строительства</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <button className={style.buttonDesc}>УЗНАТЬ БОЛЬШЕ</button>
+                    </div>
                 </div>
             </div>
         </div>
