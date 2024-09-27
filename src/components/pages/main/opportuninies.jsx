30c27627fa284415809d957116c05ac3e80d8edd
Bob <noname.from.milkyway@gmail.com>
2024-09-27 11:02:06 +0300

    opportunities block made

--- src/components/pages/main/opportuninies.jsx
+++ src/components/pages/main/opportuninies.jsx
@@ -1,13 +1,35 @@
 import style from "./opportuninies.module.css";
+import { BsFileEarmarkBarGraph } from "react-icons/bs";
 
 export default function opportuninies() {
     return (
         <div className={style.container}>
             <div className={style.c1}>
-                <h2>ЧТО ВЫ ПОЛУЧИТЕ, <br/>УСТАНОВИВ СЭС?</h2>
+                <div className={style.c1top}>
+                    <h2>ЧТО ВЫ ПОЛУЧИТЕ, УСТАНОВИВ СЭС?</h2>
+                    <div>
+                        <p>30 ЛЕТ</p>
+                        <h3>Срок службы СЭС</h3>
+                    </div>
+                </div>
+                <div className={style.c1bottom}></div>
             </div>
-            <div className={style.c2}></div>
-            <div className={style.c3}></div>
+            <div className={style.c2}>
+                <div className={style.c2top}>
+                    <h3>СЭС окупается:</h3>
+                    <p>В южных регионах от <span style={{color: '#ff8c00'}}>4-6 лет</span></p>
+                    <p>В средней полосе от <span style={{color: '#ff8c00'}}>8-9 лет</span></p>
+                    <p>В северных регионах от <span style={{color: '#ff8c00'}}>10-11 лет</span></p>
+                </div>
+                <div className={style.c2bottom}></div>
+            </div>
+            <div className={style.c3}>
+                <div className={style.c3content}>
+                    <div className={style.c3top}><BsFileEarmarkBarGraph /></div>
+                    <h3>Окупаемость инвестиций</h3>
+                    <p>По истечению срока <br/>окупаемости <b>увеличиваете <br/>свою прибыль</b></p>
+                </div>
+            </div>
         </div>
     );
 }
