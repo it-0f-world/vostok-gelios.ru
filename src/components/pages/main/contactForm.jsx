ed71738eae00eab24040572a6d23361804cda0cc
qoo <noname.from.milkyway@gmail.com>
2024-10-18 12:55:09 +0300

    contact form design completed

--- src/components/pages/main/contactForm.jsx
+++ src/components/pages/main/contactForm.jsx
@@ -2,6 +2,8 @@ import style from './contactForm.module.css';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { LuImagePlus } from "react-icons/lu";
+import ContactFormAdditional from './contactFormAdditional';
 
 export default function ContactForm() {
     const { register, handleSubmit, reset, formState: { errors } } = useForm();
@@ -37,47 +39,60 @@ export default function ContactForm() {
     }
     return (
         <div className={style.wrapper}>
-            <div className={style.container}>
-                <h2>Отправить заявку:</h2>
-                <div className={style.containerForm}>
+            <div className={style.container}>   
+                <h2>Обратная связь</h2>
                     <div className={style.contactForm}>
                         <form onSubmit={handleSubmit(onSubmitForm)}>
-                            <div className={style.Row}>
-                                <input
-                                    type="text"
-                                    name="name"
-                                    {...register("name", { required: { value: true, message: 'Вам нужно ввести ваше имя' } })}
-                                    placeholder="Как к Вам обращаться?"
-                                />
-                                <span>{errors?.name?.message}</span>
+                            <div className={style.combineRow}>
+                                <div className={style.Row}>
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        {...register("name", { required: { value: true, message: 'Вам нужно ввести ваше имя' } })}
+                                        placeholder="Как к Вам обращаться?"
+                                    />
+                                    <span>{errors?.name?.message}</span>
+                                </div>
+                                <div className={style.Row}>
+                                    <input
+                                        type="text"
+                                        name="email"
+                                        {...register("email", {
+                                            required: { value: false, message: 'Вам нужно ввести ваш e-mail адрес' },
+                                            minLength: { value: 7, message: 'Не меньше 7 символов' },
+                                            maxLength: { value: 120, message: 'Слишком много символов' },
+                                            pattern: { value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, message: 'Введите почту в правильном формате' }
+                                        })}
+                                        placeholder="Электронная почта"
+                                    />
+                                    <span>{errors?.email?.message}</span>
+                                </div>
                             </div>
-                            <div className={style.Row}>
-                                <input
-                                    type="text"
-                                    name="email"
-                                    {...register("email", {
-                                        required: { value: false, message: 'Вам нужно ввести ваш e-mail адрес' },
-                                        minLength: { value: 7, message: 'Не меньше 7 символов' },
-                                        maxLength: { value: 120, message: 'Слишком много символов' },
-                                        pattern: { value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, message: 'Введите почту в правильном формате' }
-                                    })}
-                                    placeholder="Электронная почта"
-                                />
-                                <span>{errors?.email?.message}</span>
-                            </div>
-                            <div className={style.Row}>
-                                <input
-                                    type="phone"
-                                    name="phone"
-                                    {...register("phone", {
-                                        required: { value: true, message: "Вам нужно ввести ваш номер телефона" },
-                                        minLength: { value: 4, message: "Телефон должен быть длинее 4 символов" },
-                                        maxLength: { value: 21, message: 'Слишком много цифр' },
-                                        pattern: { value: /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g, message: "Неверный формат" },
-                                    })}
-                                    placeholder="Телефон"
-                                />
-                                <span>{errors?.phone?.message}</span>
+                            <div className={style.combineRow1}>
+                                <div className={style.Row}>
+                                    <input
+                                        type="phone"
+                                        name="phone"
+                                        {...register("phone", {
+                                            required: { value: true, message: "Вам нужно ввести ваш номер телефона" },
+                                            minLength: { value: 4, message: "Телефон должен быть длинее 4 символов" },
+                                            maxLength: { value: 21, message: 'Слишком много цифр' },
+                                            pattern: { value: /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g, message: "Неверный формат" },
+                                        })}
+                                        placeholder="Телефон"
+                                    />
+                                    <span>{errors?.phone?.message}</span>
+                                </div>
+                                <div className={style.RowButtonChoose}>
+                                        <label htmlFor="picture" className={style.customFileLabel}><LuImagePlus /> загрузить фото</label>
+                                        <input
+                                            type="file"
+                                            id="picture"
+                                            name="picture"
+                                            {...register("picture", { required: false })}
+                                            className={style.hiddenFileInput}
+                                        />
+                                </div>
                             </div>
                             <div className={style.Row}>
                                 <textarea
@@ -92,38 +107,13 @@ export default function ContactForm() {
                                     placeholder="Сообщение"></textarea>
                                 <span>{errors?.message?.message}</span>
                             </div>
-                            <div className={style.RowButtonChoose}>
-                                <label htmlFor="picture" className={style.customFileLabel}>Файл изображений...</label>
-                                <input
-                                    type="file"
-                                    id="picture"
-                                    name="picture"
-                                    {...register("picture", { required: false })}
-                                    className={style.hiddenFileInput}
-                                />
-                            </div>
-                            
+                                <div className={style.RowButtonSend}>
+                                    <button className={style.primary}>Отправить Заявку</button>
+                                </div>
                         </form>
                     </div>
-                    <div className={style.rightContainer}>
-                        <div className={style.rightContainerItem}>
-                            <h3>12</h3>
-                            <p>Реализованных проектов</p>
-                        </div>
-                        <div className={style.rightContainerItem}>
-                            <h3>28МВт</h3>
-                            <p>Установленных мощностей</p>
-                        </div>
-                        <div className={style.rightContainerItem}>
-                            <h3>31 415м²</h3>
-                            <p>Площадь спроектировнных СЭС</p>
-                        </div>
-                    </div>
-                </div>
-                <div className={style.RowButtonSend}>
-                    <button className={style.primary}>Отправить Заявку</button>
-                </div>
             </div>
+            <ContactFormAdditional />
         </div>
     );
 }
---
+++ src/components/pages/main/contactFormAdditional.jsx
@@ -0,0 +1,32 @@
+import style from './contactFormAdditional.module.css';
+import { FiMapPin, FiPhone, FiMail } from "react-icons/fi";
+
+export default function ContactFormAdditional() {
+    return (
+        <div className={style.container}>
+            <h3>КОНТАКТНЫЕ ДАННЫЕ</h3>
+            <div className={style.row}>
+                <div className={style.icon}><FiMapPin /></div>
+                <div className={style.content}>
+                    <p><strong>Адрес: </strong> 143930, Московская обл., г. Балашиха, мкр. Салтыковка, ш. Разинское, д. 69, помещ. 171, офис 522А</p>
+                </div>
+            </div>
+            <div className={style.row}>
+                <div className={style.icon}><FiPhone /></div>
+                <div className={style.content}>
+                    <p><strong>Телефон: </strong> +7 (495) 123-45-67</p>
+                </div>
+            </div>
+            <div className={style.row}>
+                <div className={style.icon}><FiMail /></div>
+                <div className={style.content}>
+                    <p><strong>Почта: </strong> info@vostok-gelios.ru</p>
+                </div>
+            </div>
+            <div className={style.rules} style={{ display: 'flex', alignItems: 'flex-start' }}>
+                <div style={{ backgroundColor: '#fff', width: '8px', height: '100%', marginRight: '10px' }}></div>
+                <p style={{textAlign: 'justify'}}>*Отправляя сведения через форму обратной связи вы соглашаетесь с правилами обрабоки персональных данных. Обращение к Вам и телефонный номер обязательны для заполнения. Все данные, указанные на этой странице, не являются публичной офертой.</p>
+            </div>
+        </div>
+    )
+}
