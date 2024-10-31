22c383461be89cb6ac4fdec8dcc9c1f2d590200b
qoo <noname.from.milkyway@gmail.com>
2024-10-31 08:49:32 +0300

    burger menu links maintenance

--- src/components/burgerMenu/BurgerMenu.jsx
+++ src/components/burgerMenu/BurgerMenu.jsx
@@ -6,21 +6,21 @@ import { FiPhoneCall, FiMail } from "react-icons/fi";
 import './burgerMenu.css';
 import LogoWhite from '../../assets/images/logo/Logo-white-pure.svg';
 
-const BurgerMenu = (props) => {
+const BurgerMenu = ({ closeMenu, isOpen, ...props }) => {
     return (
-        <Menu {...props}>
+        <Menu  isOpen={isOpen} {...props}>
           <div className='bm-logo'>
-            <NavLink to="/">
+            <NavLink to="/" onClick={closeMenu}>
               <img src={LogoWhite} alt="Logo" style={{ width: '16rem' }} />
             </NavLink>
               <h4 style={{ textTransform: 'uppercase', fontWeight: 'normal', fontSize: '1.7rem' }}>Восток Гелиос</h4>
           </div>
           <div>
-            <NavLink to="/" className={({ isActive }) => isActive ? "bm-item-active" : ""}><GrMemory style={{ fontSize: '2.1rem', marginBottom: '-.4rem' }}/> Солнечные электростанции</NavLink>
-            <NavLink to="/industrial" className={({ isActive }) => isActive ? "bm-item-active" : ""}><GrServers style={{ paddingRight: '.25rem' }}/> Предприятиям</NavLink>
-            <NavLink to="/household" className={({ isActive }) => isActive ? "bm-item-active" : ""}><GrSelection style={{ paddingRight: '.25rem' }}/> Домовладениям</NavLink>
-            <NavLink to="/portfolio" className={({ isActive }) => isActive ? "bm-item-active" : ""}><GrCatalog style={{ paddingRight: '.25rem' }}/> Портфолио</NavLink>
-            <NavLink to="/contacts" className={({ isActive }) => isActive ? "bm-item-active" : ""}><BiMap style={{ paddingRight: '.25rem' }}/> Контакты</NavLink>
+            <NavLink to="/" className={({ isActive }) => isActive ? "bm-item-active" : ""} onClick={closeMenu}><GrMemory style={{ fontSize: '2.1rem', marginBottom: '-.4rem' }}/> Солнечные электростанции</NavLink>
+            <NavLink to="/industrial" className={({ isActive }) => isActive ? "bm-item-active" : ""} onClick={closeMenu}><GrServers style={{ paddingRight: '.25rem' }}/> Предприятиям</NavLink>
+            <NavLink to="/household" className={({ isActive }) => isActive ? "bm-item-active" : ""} onClick={closeMenu}><GrSelection style={{ paddingRight: '.25rem' }}/> Домовладениям</NavLink>
+            <NavLink to="/portfolio" className={({ isActive }) => isActive ? "bm-item-active" : ""} onClick={closeMenu}><GrCatalog style={{ paddingRight: '.25rem' }}/> Портфолио</NavLink>
+            <NavLink to="/contacts" className={({ isActive }) => isActive ? "bm-item-active" : ""} onClick={closeMenu}><BiMap style={{ paddingRight: '.25rem' }}/> Контакты</NavLink>
           </div>
           <div className='bm-address'>
             <a href="mailto:info@vostok-gelios.ru"><FiMail style={{ fontSize: '1.5em', marginBottom: '-.2em'}} /> info@vostok-gelios.ru</a>
--- src/components/pages/portfolio/portfolio.jsx
+++ src/components/pages/portfolio/portfolio.jsx
@@ -1,9 +1,20 @@
+import style from './portfolio.module.css'
+
 export default function About() {
     return (
-        <div>
-            <p style={{
-                color: 'white'
-            }}>POTFOLIO</p>
+        <div className={style.container}>
+            <h1>Наши проекты</h1>
+            <p>Мы нарабатываем и реализуем опыт для удовлетворения индивидуальных потребностей каждого клиента в альтернативной энергетике.</p>
+            <div className={style.objects}>
+                <div className={style.item}>
+                </div>
+                <div className={style.item}>
+                </div>
+                <div className={style.item}>
+                </div>
+                <div className={style.item}>
+                </div>
+            </div>
         </div>
     )
 }
