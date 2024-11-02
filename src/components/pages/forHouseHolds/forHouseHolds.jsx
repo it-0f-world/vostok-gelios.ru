687e7d43c4e52ac613fc1286f9192b35820683f7
qoo <noname.from.milkyway@gmail.com>
2024-11-02 11:20:52 +0300

    preliminarily general pages completed

--- src/components/pages/forHouseHolds/forHouseHolds.jsx
+++ src/components/pages/forHouseHolds/forHouseHolds.jsx
@@ -1,9 +1,26 @@
+import Header from './header'
+import Profitability from './profitability'
+import HowItWorks from './howItWorks'
+import InstallationStages from '../forIndustrial/installationStages'
+import HireUs from '../forIndustrial/hireUs'
+import WhyUs from '../forIndustrial/whyUs'
+import Protfolio from '../main/portfolio'
+import ContactForm from '../main/contactForm'
+
+
 export default function Articles() {
     return (
         <div>
-            <p style={{
-                color: 'white'
-            }}>HOUSEHOLDS</p>
+            <Header />
+            <Profitability />
+            <HowItWorks />
+            <InstallationStages />
+            <HireUs />
+            <WhyUs />
+            <Protfolio />
+            <div  style={{padding: '1.6rem 0 0'}}>
+                <ContactForm />
+            </div>
         </div>
     )
 }
--- src/components/pages/portfolio/portfolio.jsx
+++ src/components/pages/portfolio/portfolio.jsx
@@ -1,20 +1,109 @@
 import style from './portfolio.module.css'
 
+import Vladimir from "../../../assets/images/portfolio/vladimir/gallery1.webp";
+import Voronezh from "../../../assets/images/portfolio/voronezh_airport/gallery.webp";
+import Ivanovo from "../../../assets/images/portfolio/ivanovo/gallery.webp";
+import Lukoil from "../../../assets/images/portfolio/lukoil/gallery.webp";
+import Sber from "../../../assets/images/portfolio/sbercity/gallery.webp";
+import Hohland from "../../../assets/images/portfolio/hohland/gallery.webp";
+import Moscow from "../../../assets/images/portfolio/moscow_household/gallery.webp";
+import Rosinki from "../../../assets/images/portfolio/rosinki/gallery.webp";
+
 export default function About() {
     return (
-        <div className={style.container}>
-            <h1>Наши проекты</h1>
-            <p>Мы нарабатываем и реализуем опыт для удовлетворения индивидуальных потребностей каждого клиента в альтернативной энергетике.</p>
-            <div className={style.objects}>
-                <div className={style.item}>
+        <div className={style.wrapper} >
+            <div className={style.container}>
+                <div className={style.slogan}>
+                    <div className={style.yellowCube}></div>
+                    <div>
+                        <h1>Наши проекты</h1>
+                        <p>Мы нарабатываем и реализуем опыт для удовлетворения индивидуальных потребностей каждого клиента в альтернативной энергетике.</p>
+                    </div>
                 </div>
-                <div className={style.item}>
-                </div>
-                <div className={style.item}>
-                </div>
-                <div className={style.item}>
+                <div className={style.objects}>
+                    <div className={style.item}>
+                        <div className={style.imageContainer}>
+                            <img src={Vladimir} alt="солнечная электростанция во Владимире для частного дома" />
+                            <div className={style.info} >
+                                <h3 className={style.hightlight}>г. Владимир, частный дом</h3>
+                                <p className={style.hightlight}>23 кВт. Расчётная выработка 19,5 МВт·ч/год</p>
+                            </div>
+                            <div className={style.overlay}></div>
+                        </div>
+                    </div>
+                    <div className={style.item}>
+                        <div className={style.imageContainer}>
+                            <img src={Voronezh} alt="солнечная электростанция в аэропорту Воронежа" />
+                            <div className={style.info}>
+                                <h3 className={style.hightlight}>г. Воронеж, аэропорт</h3>
+                                <p className={style.hightlight}>102,4 кВт. Расчётная выработка 102 МВт·ч/год</p>
+                            </div>
+                            <div className={style.overlay}></div>
+                        </div>
+                    </div>
+                    <div className={style.item}>
+                        <div className={style.imageContainer}>
+                            <img src={Ivanovo} alt="солнечная электростанция в Иваново для кондитерской фабрики" />
+                            <div className={style.info}>
+                                <h3 className={style.hightlight}>г. Иваново, кондитерская фабрика</h3>
+                                <p className={style.hightlight}>155,52 кВт. Расчётная выработка 127,5 МВт·ч/год</p>
+                            </div>
+                            <div className={style.overlay}></div>
+                        </div>
+                    </div>
+                    <div className={style.item}>
+                        <div className={style.imageContainer}>
+                            <img src={Lukoil} alt="солнечная электростанция для Лукойл в Москве" />
+                            <div className={style.info}>
+                                <h3 className={style.hightlight}>г. Москва, офис Лукоил</h3>
+                                <p className={style.hightlight}>59,3 кВт. Расчётная выработка 47 МВт·ч/год</p>
+                            </div>
+                            <div className={style.overlay}></div>
+                        </div>
+                    </div>
+                    <div className={style.item}>
+                        <div className={style.imageContainer}>
+                            <img src={Sber} alt="солнечная электростанция для ЖК Сберсити в Москве" />
+                            <div className={style.info}>
+                                <h3 className={style.hightlight}>г. Москва, ЖК Сберсити</h3>
+                                <p className={style.hightlight}>115,2 кВт. Фасадные и крышные СЭС</p>
+                            </div>
+                            <div className={style.overlay}></div>
+                        </div>
+                    </div>
+                    <div className={style.item}>
+                        <div className={style.imageContainer}>
+                            <img src={Hohland} alt="солнечная электростанция для Хохланд в Москве" />
+                            <div className={style.info}>
+                                <h3 className={style.hightlight}>г. Москва, производство Хохланд</h3>
+                                <p className={style.hightlight}>117,5 кВт. Двухсторонние фотоэлектрические модули</p>
+                            </div>
+                            <div className={style.overlay}></div>
+                        </div>
+                    </div>
+                    <div className={style.item}>
+                        <div className={style.imageContainer}>
+                            <img src={Moscow} alt="солнечная электростанция в Красногорске, частный дом" />
+                            <div className={style.info}>
+                                <h3 className={style.hightlight}>г. Красногорск, частный дом</h3>
+                                <p className={style.hightlight}>25,9 кВт. Расчётная выработка 18,1 МВт·ч/год</p>
+                            </div>
+                            <div className={style.overlay}></div>
+                        </div>
+                    </div>
+                    <div className={style.item}>
+                        <div className={style.imageContainer}>
+                            <img src={Rosinki} alt="солнечная электростанция для частного дома в г.Москва" />
+                            <div className={style.info}>
+                                <h3 className={style.hightlight}>г. Москва, частный дом</h3>
+                                <p className={style.hightlight}>13,4 кВт. Расчётная выработка 11,4 МВт·ч/год</p>
+                            </div>
+                            <div className={style.overlay}></div>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <div className={style.footer}></div>
         </div>
     )
 }
