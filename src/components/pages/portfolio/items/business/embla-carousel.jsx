d8043bbb6ead8979e43634d3c8a2d135a62802af
qoo <noname.from.milkyway@gmail.com>
2024-11-05 13:48:03 +0300

    embla carousel finished

--- src/components/pages/portfolio/items/business/embla-carousel.jsx
+++ src/components/pages/portfolio/items/business/embla-carousel.jsx
@@ -29,7 +29,7 @@ import IMG_0567 from '../../img/voronezh_airport/IMG_0567.webp'
 import IMG_0578 from '../../img/voronezh_airport/IMG_0578.webp'
 
 export default function Carousel(props) {
-    const { slides, options } = props
+    const { options } = props
     const [emblaRef, emblaApi] = useEmblaCarousel(options)
 
     const { selectedIndex, scrollSnaps, onDotButtonClick } =
@@ -62,6 +62,125 @@ export default function Carousel(props) {
                         <div className="embla__slide">
                             <div className="embla__slide__number">
                                 <img src={IMG_0508} alt="Монтаж солнечных панелей на плоской крыше. Город Воронеж, Аэропорт. Солнечная электростанция для бизнеса." />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="embla__slide">
+                        <div className="embla__slide">
+                            <div className="embla__slide__number">
+                                <img src={IMG_0509} alt="Монтаж солнечных панелей на плоской крыше. Город Воронеж, Аэропорт. Солнечная электростанция для бизнеса." />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="embla__slide">
+                        <div className="embla__slide">
+                            <div className="embla__slide__number">
+                                <img src={IMG_0510} alt="Монтаж солнечных панелей на плоской крыше. Город Воронеж, Аэропорт. Солнечная электростанция для бизнеса." />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="embla__slide">
+                        <div className="embla__slide">
+                            <div className="embla__slide__number">
+                                <img src={IMG_0511} alt="Монтаж солнечных панелей на плоской крыше. Город Воронеж, Аэропорт. Солнечная электростанция для бизнеса." />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="embla__slide">
+                        <div className="embla__slide">
+                            <div className="embla__slide__number">
+                                <img src={IMG_0555} alt="Монтаж солнечных панелей на плоской крыше. Город Воронеж, Аэропорт. Солнечная электростанция для бизнеса." />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="embla__slide">
+                        <div className="embla__slide">
+                            <div className="embla__slide__number">
+                                <img src={IMG_0556} alt="Монтаж солнечных панелей на плоской крыше. Город Воронеж, Аэропорт. Солнечная электростанция для бизнеса." />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="embla__slide">
+                        <div className="embla__slide">
+                            <div className="embla__slide__number">
+                                <img src={IMG_0557} alt="Монтаж солнечных панелей на плоской крыше. Город Воронеж, Аэропорт. Солнечная электростанция для бизнеса." />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="embla__slide">
+                        <div className="embla__slide">
+                            <div className="embla__slide__number">
+                                <img src={IMG_0558} alt="Монтаж солнечных панелей на плоской крыше. Город Воронеж, Аэропорт. Солнечная электростанция для бизнеса." />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="embla__slide">
+                        <div className="embla__slide">
+                            <div className="embla__slide__number">
+                                <img src={IMG_0559} alt="Монтаж солнечных панелей на плоской крыше. Город Воронеж, Аэропорт. Солнечная электростанция для бизнеса." />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="embla__slide">
+                        <div className="embla__slide">
+                            <div className="embla__slide__number">
+                                <img src={IMG_0560} alt="Монтаж солнечных панелей на плоской крыше. Город Воронеж, Аэропорт. Солнечная электростанция для бизнеса." />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="embla__slide">
+                        <div className="embla__slide">
+                            <div className="embla__slide__number">
+                                <img src={IMG_0561} alt="Монтаж солнечных панелей на плоской крыше. Город Воронеж, Аэропорт. Солнечная электростанция для бизнеса." />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="embla__slide">
+                        <div className="embla__slide">
+                            <div className="embla__slide__number">
+                                <img src={IMG_0562} alt="Монтаж солнечных панелей на плоской крыше. Город Воронеж, Аэропорт. Солнечная электростанция для бизнеса." />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="embla__slide">
+                        <div className="embla__slide">
+                            <div className="embla__slide__number">
+                                <img src={IMG_0563} alt="Монтаж солнечных панелей на плоской крыше. Город Воронеж, Аэропорт. Солнечная электростанция для бизнеса." />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="embla__slide">
+                        <div className="embla__slide">
+                            <div className="embla__slide__number">
+                                <img src={IMG_0564} alt="Монтаж солнечных панелей на плоской крыше. Город Воронеж, Аэропорт. Солнечная электростанция для бизнеса." />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="embla__slide">
+                        <div className="embla__slide">
+                            <div className="embla__slide__number">
+                                <img src={IMG_0565} alt="Монтаж солнечных панелей на плоской крыше. Город Воронеж, Аэропорт. Солнечная электростанция для бизнеса." />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="embla__slide">
+                        <div className="embla__slide">
+                            <div className="embla__slide__number">
+                                <img src={IMG_0566} alt="Монтаж солнечных панелей на плоской крыше. Город Воронеж, Аэропорт. Солнечная электростанция для бизнеса." />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="embla__slide">
+                        <div className="embla__slide">
+                            <div className="embla__slide__number">
+                                <img src={IMG_0567} alt="Монтаж солнечных панелей на плоской крыше. Город Воронеж, Аэропорт. Солнечная электростанция для бизнеса." />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="embla__slide">
+                        <div className="embla__slide">
+                            <div className="embla__slide__number">
+                                <img src={IMG_0578} alt="Монтаж солнечных панелей на плоской крыше. Город Воронеж, Аэропорт. Солнечная электростанция для бизнеса." />
                             </div>
                         </div>
                     </div>
--- src/components/pages/portfolio/items/business/voronezh.jsx
+++ src/components/pages/portfolio/items/business/voronezh.jsx
@@ -2,13 +2,12 @@ import style from '../portfolio-items.module.css'
 import Carousel from './embla-carousel.jsx'
 
 const OPTIONS = {}
-const SLIDE_COUNT = 5
-const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
 export default function Voronezh(props) {
     return (
         <div className={style.wrapper}>
-            <Carousel slides={SLIDES} options={OPTIONS} />
+            <h1>г. Воронеж, Аэропорт. <span style={{color: '#ff8c00'}}>СЭС</span> на плоской кровле.</h1>
+            <Carousel options={OPTIONS} />
         </div>
     )
 }
